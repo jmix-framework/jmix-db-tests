decc6752ea6e1cf1aa436c0cfd332128d73587d1
Artem Artemev <a.artemev@haulmont.com>
2022-03-02 14:03:28 +0400

    Fix Firebird test Haulmont/jmix-db-tests#3
    Remove creating DATABASECHANGELOGLOCK table statemant cause it is fixed already

--- src/test/java/io/jmix/dbtests/init/FirebirdInitializer.java
+++ src/test/java/io/jmix/dbtests/init/FirebirdInitializer.java
@@ -5,11 +5,6 @@ import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class FirebirdInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
     static FirebirdContainer<?> container = new FirebirdContainer<>()
@@ -26,14 +21,5 @@ public class FirebirdInitializer implements ApplicationContextInitializer<Config
                 "abc.datasource.password=" + container.getPassword(),
                 "jmix.data.dbmsType_abc = firebird"
         ).applyTo(applicationContext.getEnvironment());
-
-        // todo remove after https://github.com/liquibase/liquibase/issues/1303 is fixed
-        try (Connection connection = DriverManager.getConnection(container.getJdbcUrl(), container.getUsername(), container.getPassword());
-             Statement stmt = connection.createStatement()) {
-            stmt.execute("CREATE TABLE DATABASECHANGELOGLOCK (ID VARCHAR(255) NOT NULL, LOCKED SMALLINT NOT NULL, LOCKGRANTED TIMESTAMP, LOCKEDBY VARCHAR(255))");
-        } catch (SQLException e) {
-            //noinspection JmixRuntimeException
-            throw new RuntimeException(e);
-        }
     }
 }
